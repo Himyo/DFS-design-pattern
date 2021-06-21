12df83f042a06ba326553c66400b7e34d586047b
Franck <franck.patouillar@permisdesauver.fr>
2021-06-21 15:04:03 +0200

    new modif

--- src/Models/Database.php
+++ src/Models/Database.php
@@ -7,20 +7,15 @@ class Database {
     public static function getInstance()
     {
         if (!Database::$instance) {
-            $user = 'root';
-            $pass = 'root';
-            return Database::$instance = new \PDO('mysql:host=localhost;dbname=db', $user, $pass);
-        } else {
-            return null;
+            try {
+                $user = 'root';
+                $pass = 'root';
+                return Database::$instance = new \PDO('mysql:host=localhost;dbname=db', $user, $pass);
+
+            }catch (\Exception $e){
+                var_dump($e->getMessage());
+            }
         }
-    }
-    public function query($sql){
-        // Par exemple, toutes les requêtes sur la base de
-        // données d’une application passent par cette méthode.
-        // Par conséquent, vous pouvez définir le code des
-        // limitations ou de la mise en cache ici.
-        // ...
-        $result = self::$instance->query($sql);
-         return $result->fetchAll();
+        return null;
     }
 }
--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
 require 'autoload.php';
 use Models\Database;
-var_dump(Database::getInstance("localhost","db","root","root"));
+var_dump(Database::getInstance());
 echo 'Hello';
